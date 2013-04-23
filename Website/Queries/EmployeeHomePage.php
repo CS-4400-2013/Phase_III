ea390e17297c68275930221a18298c4ee3a6a0bd
Kevin <kshu7@gatech.edu>
2013-04-23 19:55:10 -0400

    Fixed some links between pages

--- Website/Queries/EmployeeHomePage.php
+++ Website/Queries/EmployeeHomePage.php
@@ -11,50 +11,30 @@ if (mysqli_connect_errno($connection))
 }
 
 $Employee_Next = $_GET["Employee_Next"];
-$reporttype=$_GET["Employee_Report"]
+$reporttype=$_GET["Employee_report"];
 $username = $_SESSION['username'];
 
-// Check if member has this username
-$member_query = mysqli_query($connection,"SELECT username FROM member WHERE username='".$username."'");
-if(!$member_query)
-	echo "query error";
-
-$member_result = mysqli_fetch_array($member_query);
 switch($Employee_Next)
 {
-	case "Manage_car":
-		if(isset($member_result['username']))
-			header('Location: ../managecars.html');
-			break;
-		else
-			header('Location: ../MemberHomePage.html');
-			break;
+	case "Manage_Car":
+		header('Location: ../managecars.html');
+		break;
 	case "Maintainence_Requests":
 		header('Location: servicerequest.php');
 		break;
 	case "Rental_Request_Change":
-		if(isset($member_result['username'])) 
-			header('Location: RentalRequestChangeUI.php');
-			break;
-		else
-			header('Location: ../MemberHomePage.html');
-			break;
+		header('Location: RentalRequestChangeUI.php');
+		break;
 	case "Reports":
-		if(isset($member_result['username']))
-			if($reporttype="Location_Preference")
-				header('Location: EmployeeReportLocationPreference.php');
-			elseif($reporttype="Frequent_Users")
-				header('Location: EmployeeReportFrequentUsers.php');
-				break;
-			elseif($reporttype="Maintainence_History_Report")
-				header('Location: EmployeeReportMaintainenceHistory.php');
-				break;
-			else
-				header('Location: ../MemberHomePage.html');
-				break;
+		if($reporttype=="Location_preference")
+			header('Location: EmployeeReportLocationPreference.php');
+		elseif($reporttype=="Frequent_Users")
+			header('Location: EmployeeReportFrequentUsers.php');
+		elseif($reporttype=="Maintainence_History_Report")
+			header('Location: EmployeeReportMaintainenceHistory.php');
 		else
-			header('Location: ../MemberHomePage.html');
-			break;
+			header('Location: ../EmployeeHomePage.html');
+		break;
 }
 
 mysqli_close($connection);
--- Website/Queries/managecars.php
+++ Website/Queries/managecars.php
@@ -11,22 +11,12 @@ if (mysqli_connect_errno($connection))
 }
 
 $Employee_Next = $_GET["Employee_Next"];
-$username = $_SESSION['username'];
 
-// Check if member has this username
-$member_query = mysqli_query($connection,"SELECT username FROM member WHERE username='".$username."'");
-if(!$member_query)
-	echo "query error";
-
-$member_result = mysqli_fetch_array($member_query);
 switch($Employee_Next)
 {
 	case "Add_Car":
-		if(isset($member_result['username']))
-			header('Location: ../AddCar.html');
-		else
-			header('Location: ../MemberHomePage.html');
-			break;
+		header('Location: ../AddCar.html');
+		break;
 	case "ChangeCarLocations":
 		header('Location: ChangeCarLocation.php');
 		break;
